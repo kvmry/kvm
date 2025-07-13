aa494d30f7ed578f28101870166595d92cda6d82
alzahrakulttuurikeskus <alzahrakulttuurikeskus@gmail.com>
2025-07-13 08:30:08 +0000

    Initial repository

---
+++ src/bootstrap.js
@@ -0,0 +1,12 @@
+window["APP_TEMPLATE_BOOTSTRAP"] = {
+    geminiApiKey: "AIzaSyDG0V1pVmdR7R_7kkXzf2uKTzM-oKbnWWM",
+
+    firebase: {
+      apiKey: "AIzaSyBrhWLY08Da8x8y5SHzF4z6eBJaCGaPwDc",
+      authDomain: "kvmry-e88cb.firebaseapp.com",
+      projectId: "kvmry-e88cb",
+      storageBucket: "kvmry-e88cb.firebasestorage.app",
+      messagingSenderId: "769540772139",
+      appId: "1:769540772139:web:4ffc74c569a5d5b78ef56e",
+    }
+    };
